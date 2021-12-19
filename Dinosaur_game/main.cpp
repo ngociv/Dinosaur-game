38d4d918753d43a606e564a06bd23cbe2dbc1da5
Ngoc in Vietnam <ngocnviv@gmail.com>
2021-12-19 11:27:45 +0700

    Almost done

--- Dinosaur_game/main.cpp
+++ Dinosaur_game/main.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <iomanip>
 #include <SDL.h>
 #include <SDL_image.h>
+#include <SDL_ttf.h>
 
 #undef main
 
 using namespace std;
 
+bool g_bRunGame = false;
 bool g_bMove = false;
 
 const int SCREEN_WIDTH = 840;
@@ -21,16 +25,12 @@ const string WINDOW_TITLE = "Dinosaur game by Ngociv";
 
 unsigned long g_nTime = 0;
 unsigned long g_nSpeedGame = 2;
+unsigned long g_nDistance = 0;
+int g_nScorePoint = 0;
 
 SDL_Window* g_pWindow = nullptr;
 SDL_Renderer* g_pRenderer = nullptr;
 SDL_Event* g_pEvent = new SDL_Event;
-
-void initSDL(SDL_Window*& pWindow, SDL_Renderer*& pRenderer);
-void quitSDL(SDL_Window*& pWindow, SDL_Renderer*& pRenderer);
-void loadImage(string file_name, SDL_Renderer* pRenderer, const SDL_Rect* srcrect, const SDL_Rect* dstrect);
-void runGame();
-SDL_Texture* loadTexture(string file_name, SDL_Renderer* pRenderer);
 
 struct point_2d
 {
@@ -55,7 +55,23 @@ struct polygon
 	point_2d tail;
 };
 
-class objectMoving
+struct colorRGB
+{
+	int r;
+	int g;
+	int b;
+};
+
+colorRGB orange = { 255,165,0 };
+colorRGB darkSeaGreen4 = { 105,139,105 };
+
+void initSDL(SDL_Window*& pWindow, SDL_Renderer*& pRenderer);
+void quitSDL(SDL_Window*& pWindow, SDL_Renderer*& pRenderer);
+void openingGame();
+void runGame();
+SDL_Texture* loadTexture(string file_name, SDL_Renderer* pRenderer);
+
+class object
 {
 protected:
 	string file_name;
@@ -64,14 +80,14 @@ protected:
 	SDL_Rect* pDstRect;
 
 public:
-	objectMoving()
+	object()
 	{
 		pSrcRect = nullptr;
 		pDstRect = nullptr;
 		pTexture = nullptr;
 	}
 
-	~objectMoving()
+	~object()
 	{
 		if (pSrcRect)
 		{
@@ -133,9 +149,21 @@ public:
 	virtual void render() = 0;
 };
 
-class character : public objectMoving
+class character : public object
 {
 public:
+	void appear()
+	{
+		if (pDstRect->y < 257)
+		{
+			pDstRect->y++;
+			SDL_RenderCopy(g_pRenderer, pTexture, pSrcRect, pDstRect);
+		}
+		else
+		{
+			g_bRunGame = true;
+		}
+	}
 	virtual void render()
 	{
 		if (!g_bMove){
@@ -178,9 +206,13 @@ public:
 	}
 };
 
-class background : public objectMoving
+class background : public object
 {
 public:
+	void appear()
+	{
+		SDL_RenderCopy(g_pRenderer, pTexture, pSrcRect, pDstRect);
+	}
 	virtual void render()
 	{
 		if (pDstRect->x == 0)
@@ -196,6 +228,10 @@ public:
 				pDstRect->w = SCREEN_WIDTH;
 			}
 			g_nTime++;
+			g_nDistance += g_nSpeedGame;
+			if ((g_nDistance % 80) < g_nSpeedGame) {
+				g_nScorePoint++;
+			}
 		}
 		else
 		{
@@ -213,7 +249,7 @@ public:
 	}
 };
 
-class obstacle : public objectMoving
+class obstacle : public object
 {
 private:
 	bool bAppear;
@@ -252,11 +288,74 @@ public:
 	}
 };
 
+class text : public object
+{
+private:
+	string content;
+	TTF_Font* pFont;
+	SDL_Color color;
+public:
+	text()
+	{
+		pFont = nullptr;
+	}
+	~text()
+	{
+		if (pFont)
+		{
+			TTF_CloseFont(pFont);
+			pFont = nullptr;
+		}
+	}
+	void setContent(string content)
+	{
+		this->content = content;
+	}
+	void setFont(int nSize)
+	{
+		pFont = TTF_OpenFont(file_name.c_str(), nSize);
+		if (!pFont)
+		{
+			cout << "Cannot open font! Error : " << TTF_GetError() << endl;
+			exit(1);
+		}
+	}
+	void setColor(colorRGB color)
+	{
+		this->color.r = color.r;
+		this->color.g = color.g;
+		this->color.b = color.b;
+		this->color.a = 255;
+	}
+	void setTexture()
+	{
+		SDL_Surface* pSurface = nullptr;
+		pSurface = TTF_RenderText_Solid(pFont, content.c_str(), color);
+		if (!pSurface)
+		{
+			cout << "Cannot render text solid! Error : " << TTF_GetError() << endl;
+			exit(2);
+		}
+		pTexture = SDL_CreateTextureFromSurface(g_pRenderer, pSurface);
+		if (!pTexture)
+		{
+			cout << "Cannot create texture from surface! Error : " << SDL_GetError() << endl;
+			exit(2);
+		}
+		SDL_FreeSurface(pSurface);
+		pSurface = nullptr;
+	}
+	virtual void render()
+	{
+		SDL_RenderCopy(g_pRenderer, pTexture, pSrcRect, pDstRect);
+	}
+};
+
 int main()
 {
 	initSDL(g_pWindow, g_pRenderer);
-	
-	
+
+	openingGame();
 	runGame();
 
 	quitSDL(g_pWindow, g_pRenderer);
@@ -268,6 +367,11 @@ void initSDL(SDL_Window*& pWindow, SDL_Renderer*& pRenderer)
 	if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
 	{
 		cout << "Cannot initialize SDL! Error : " << SDL_GetError() << endl;
+		exit(1);
+	}
+	if (TTF_Init() < 0)
+	{
+		cout << "Cannot initialize TTF! Error : " << TTF_GetError() << endl;
 		exit(1);
 	}
 	pWindow = SDL_CreateWindow(WINDOW_TITLE.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
@@ -293,6 +397,7 @@ void quitSDL(SDL_Window*& pWindow, SDL_Renderer*& pRenderer)
 	pRenderer = nullptr;
 	SDL_DestroyWindow(pWindow);
 	pWindow = nullptr;
+	TTF_Quit();
 	SDL_Quit();
 	return;
 }
@@ -320,25 +425,12 @@ SDL_Texture* loadTexture(string file_name, SDL_Renderer* pRenderer)
 	return pTexture;
 }
 
-void loadImage(string file_name, SDL_Renderer* pRenderer, const SDL_Rect* srcrect, const SDL_Rect* dstrect)
-{
-	SDL_Texture* pImage = loadTexture(file_name, pRenderer);
-	SDL_RenderCopy(pRenderer, pImage, srcrect, dstrect);
-	SDL_DestroyTexture(pImage);
-	pImage = nullptr;
-	return;
-}
-
-character* initCharacter()
+character* initCharacter(int dstX, int dstY, int dstW, int dstH)
 {
 	character* pCharacter = new character;
 
 	pCharacter->setName("character.png");
 
-	int dstX = 140;
-	int dstY = 257;
-	int dstW = 70;
-	int dstH = 90;
 	pCharacter->setDstRect(dstX, dstY, dstW, dstH);
 
 	pCharacter->setTexture();
@@ -364,6 +456,16 @@ obstacle* initObstacle(string file_name, int srcX, int srcY, int srcW, int srcH,
 	pObstacle->setDstRect(dstX, dstY, dstW, dstH);
 	pObstacle->setTexture();
 	return pObstacle;
+}
+
+text* initText(string file_name, colorRGB color, int nSize, int dstX, int dstY, int dstW, int dstH)
+{
+	text* pText = new text;
+	pText->setName(file_name);
+	pText->setColor(color);
+	pText->setFont(nSize);
+	pText->setDstRect(dstX, dstY, dstW, dstH);
+	return pText;
 }
 
 bool isInside(point_2d& M, rectangle* pRec)
@@ -424,6 +526,10 @@ void handleEvent()
 		{
 			g_bMove = true;
 		}
+		if (g_pEvent->key.keysym.sym == SDLK_RETURN)
+		{
+			g_bRunGame = true;
+		}
 	}
 }
 
@@ -455,9 +561,19 @@ bool isImpact(character* pCharacter, obstacle* pObs)
 	return false;
 }
 
-void stopGame()
+void gameOver()
 {
-	while (true)
+	text* pGameOver = initText("dpcomic.ttf", orange, 100, 300, 100, 350, 60);
+	pGameOver->setContent("Game Over");
+	pGameOver->setTexture();
+	pGameOver->render();
+
+	text* pPlayAgain = initText("dpcomic.ttf", orange, 100, 200, 200, 450, 65);
+	pPlayAgain->setContent("Enter to play again");
+	pPlayAgain->setTexture();
+	pPlayAgain->render();
+
+	while (!g_bRunGame)
 	{
 		SDL_RenderPresent(g_pRenderer);
 		handleEvent();
@@ -468,31 +584,75 @@ void handleImpact(character* pCharacter, obstacle* pObs_1, obstacle* pObs_2, obs
 {
 	if (isImpact(pCharacter, pObs_1))
 	{
-		stopGame();
+		g_bRunGame = false;
+		gameOver();
 	}
 	if (isImpact(pCharacter, pObs_2))
 	{
-		stopGame();
+		g_bRunGame = false;
+		gameOver();
 	}
 	if (isImpact(pCharacter, pObs_3))
 	{
-		stopGame();
+		g_bRunGame = false;
+		gameOver();
 	}
 	if (isImpact(pCharacter, pObs_4))
 	{
-		stopGame();
+		g_bRunGame = false;
+		gameOver();
+	}
+}
+
+void openingGame()
+{
+	background* pBackground = initBackground(0, 0, BACKGROUND_IMG_WIDTH, BACKGROUND_IMG_HEIGHT, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+	pBackground->appear();
+	text* pIntro_1 = initText("dpcomic.ttf", darkSeaGreen4, 100, 83, 100, 700, 50);
+	pIntro_1->setContent("Dinosaur game but the Dinosaur runs backwards");
+	pIntro_1->setTexture();
+	pIntro_1->render();
+
+	text* pIntro_2 = initText("dpcomic.ttf", orange, 100, 200, 200, 450, 65);
+	pIntro_2->setContent("Press Enter to try it");
+	pIntro_2->setTexture();
+	pIntro_2->render();
+
+	while (!g_bRunGame)
+	{
+		SDL_RenderPresent(g_pRenderer);
+		handleEvent();
+	}
+
+	g_bRunGame = false;
+	character* pCharacter = initCharacter(140, -90, 70, 90);
+	while (!g_bRunGame)
+	{
+		SDL_RenderClear(g_pRenderer);
+		pBackground->appear();
+		pCharacter->appear();
+		SDL_RenderPresent(g_pRenderer);
+		SDL_Delay(11);
+		handleEvent();
 	}
 }
 
 void runGame()
 {
-	character* pCharacter = initCharacter();
+	g_bMove = false;
+	character* pCharacter = initCharacter(140, 257, 70, 90);
 	background* pBackground_1 = initBackground(0, 0, BACKGROUND_IMG_WIDTH, BACKGROUND_IMG_HEIGHT, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
 	background* pBackground_2 = initBackground(0, 0, 0, BACKGROUND_IMG_HEIGHT, SCREEN_WIDTH, 0, 0, SCREEN_HEIGHT);
 	obstacle* pFire_1 = initObstacle("fire.png", 0, 0, 0, FIRE_IMG_HEIGHT, SCREEN_WIDTH, 270, 0, FIRE_IMG_HEIGHT);
 	obstacle* pFire_2 = initObstacle("fire.png", 0, 0, 0, FIRE_IMG_HEIGHT, SCREEN_WIDTH, 270, 0, FIRE_IMG_HEIGHT);
 	obstacle* pUFO_1 = initObstacle("ufo.png", 0, 0, 0, UFO_IMG_HEIGT, SCREEN_WIDTH, 160, 0, UFO_IMG_HEIGT);
 	obstacle* pUFO_2 = initObstacle("ufo.png", 0, 0, 0, UFO_IMG_HEIGT, SCREEN_WIDTH, 260, 0, UFO_IMG_HEIGT);
+
+	text* pScore = initText("zorque.ttf", orange, 100, 680, 20, 70, 25);
+	pScore->setContent("Score : ");
+	pScore->setTexture();
+
+	text* pScorePoint = initText("zorque.ttf", orange, 100, 750, 20, 55, 25);
 
 	while (true)
 	{
@@ -501,6 +661,13 @@ void runGame()
 		pBackground_1->render();
 		pBackground_2->render();
 		pCharacter->render();
+		pScore->render();
+
+		ostringstream scorePoint;
+		scorePoint << setw(4) << setfill('0') << g_nScorePoint;
+		pScorePoint->setContent(scorePoint.str());
+		pScorePoint->setTexture();
+		pScorePoint->render();
 
 		if (((g_nTime - 2) % 200) == 0) {
 			srand(time(NULL));
